a40504ee627720d79033a1bbf9e8bf6f405d62bb
AryanPradhan <Aryanrp2007@gmail.com>
2023-09-01 18:31:29 +0400

    Initial Commit

---
+++ StudentMentalHealth/StudentMentalHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentMentalHealthApp.swift
+//  StudentMentalHealth
+//
+//  Created by Aryan Pradhan on 01/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct StudentMentalHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StudentMentalHealthUITests/StudentMentalHealthUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StudentMentalHealthUITestsLaunchTests.swift
+//  StudentMentalHealthUITests
+//
+//  Created by Aryan Pradhan on 01/09/2023.
+//
+
+import XCTest
+
+final class StudentMentalHealthUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
