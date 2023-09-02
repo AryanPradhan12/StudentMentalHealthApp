8e12dbc2678ebcf9d2ff6a6ebec1e3f4ea1ef8b2
AryanPradhan <Aryanrp2007@gmail.com>
2023-09-02 22:27:52 +0400

    Background change for login page

--- StudentMentalHealth/ContentView.swift
+++ StudentMentalHealth/ContentView.swift
@@ -8,20 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Student mental health")
-            Text("Push testing")
-        }
-        .padding()
+        NavigationView {
+            
+        
+                ZStack{
+                    Color.purple.ignoresSafeArea()
+                    Circle()
+                        .scale(1.7)
+                        .foregroundColor(.white.opacity(0.15))
+                    Circle()
+                        .scale(1.35)
+                        .foregroundColor(.white)
+                }
+                .navigationBarHidden(true)
+            }
+        
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
