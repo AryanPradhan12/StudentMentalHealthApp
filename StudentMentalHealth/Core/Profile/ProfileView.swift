57cd46413008b24d43dabcb606a35c125270f2df
AryanPradhan <Aryanrp2007@gmail.com>
2023-09-04 17:58:09 +0400

    Test pull

--- StudentMentalHealth/Core/Profile/ProfileView.swift
+++ StudentMentalHealth/Core/Profile/ProfileView.swift
@@ -43,7 +43,11 @@ struct ProfileView: View {
             }
             
             Section("Account") {
-                
+                Button {
+                    print
+                } label: {
+                    
+                }
             }
         }
     }
---
+++ StudentMentalHealth/Core/Root/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  StudentMentalHealth
+//
+//  Created by Aryan Pradhan on 01/09/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("cheese")
+            Text("cheese.2")
+    }
+        .padding()
+    }
+    
+    
+    
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+}
+
